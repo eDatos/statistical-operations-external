466877c25bc5fefe41b9ae2c7b989ac90d7be884
amonfue <amonfue@arte-consultores.com>
2019-07-11 09:10:26 +0100

    METAMAC-2965 - Creación del proyecto statistical-operations-external
    > Se configura Thymeleaf.

--- src/main/java/es/gobcan/istac/statistical/operations/external/web/rest/DefaultController.java
+++ src/main/java/es/gobcan/istac/statistical/operations/external/web/rest/DefaultController.java
@@ -1,17 +1,11 @@
 package es.gobcan.istac.statistical.operations.external.web.rest;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 import es.gobcan.istac.statistical.operations.external.config.ApplicationProperties;
 
@@ -26,14 +20,7 @@ public class DefaultController {
 
     @RequestMapping(value = {"", "/index.html", "/**/{path:[^\\.]*}"})
     @SuppressWarnings("unchecked")
-    public ModelAndView index(HttpServletRequest request) {
-        log.debug("DefaultController index: Contextpath" + request.getContextPath() + "  ServletPath = " + request.getServletPath());
-        Map<String, Object> model = new HashMap<>();
-        model.put("metadata", applicationProperties.getMetadata());
-        Map<String, Object> flashMap = (Map<String, Object>) RequestContextUtils.getInputFlashMap(request);
-        if (flashMap != null) {
-            model.putAll(flashMap);
-        }
-        return new ModelAndView("index", model);
+    public ModelAndView index() {
+        return new ModelAndView("index");
     }
 }
