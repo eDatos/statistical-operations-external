b7ff23396ad2628dfe7a90487117b9e9ec576903
amonfue <amonfue@arte-consultores.com>
2019-07-17 10:09:26 +0100

    METAMAC-2967 - Elaborar búsqueda sobre página uno
    > Se implementa atajos de teclado en el buscador de operaciones
    estadísticas.

--- src/main/resources/static/js/operations.js
+++ src/main/resources/static/js/operations.js
@@ -2,6 +2,15 @@
 
 (function() {
 
+    var $searchResultFocused;
+    var totalSearchResult = 0;
+
+    function focusSearchResult($newFocused) {
+        $searchResultFocused && $searchResultFocused.removeClass('focused');
+        $newFocused.addClass('focused');
+        $searchResultFocused = $newFocused;
+    }
+
     function clickOutsideSearchHandler() {
         hideSearchResults();
     }
@@ -14,14 +23,62 @@
     function hideSearchResults() {
         $('.ac-search-results-panel').hide();
         $(document).off('click', clickOutsideSearchHandler);
+        document.removeEventListener('keydown', arrowKeyPressHandler);
     }
 
-    $('.search-container').on('click', function(e) {
-        if($(e.target).closest('a').length == 0) {
+    function select($result, index) {
+        var selected = index;
+        var $searchResults = $('.ac-search-results');
+        
+        var elHeight = $result.height();
+        var scrollTop = $searchResults.scrollTop();
+        var viewport = scrollTop + $searchResults.height();
+        var elOffset = elHeight * selected;
+        
+        if (elOffset < scrollTop || (elOffset + elHeight) > viewport)
+            $searchResults.scrollTop(elOffset);
+
+        focusSearchResult($result);
+    }
+
+    function arrowKeyPressHandler(e) {
+        if (!totalSearchResult || [38 /*UP*/, 40 /*DOWN*/, 13 /*ENTER*/].indexOf(e.keyCode) == -1) return;
+
+        
+        if (e.keyCode === 13) {
+            $searchResultFocused && $searchResultFocused.find('a')[0].click();
+            return;
+        }
+        
+        var iFocused = $searchResultFocused && $searchResultFocused.data('iresult');
+        if (e.keyCode === 38) { // up
+            iFocused = iFocused != null & iFocused != 0? iFocused : totalSearchResult;
+            iFocused--;
+        }
+        else if (e.keyCode === 40) { // down
+            iFocused = iFocused != null && iFocused < (totalSearchResult -1)? iFocused : - 1;
+            iFocused++;
+            
+        }
+        select($('.search-results-item[data-iresult="'+ iFocused + '"]'), iFocused);
+
+        e.preventDefault();
+    }
+
+    function showSearchresults() {
+        $('.ac-search-results-panel').show();
+        addEventClickOutsideSearch();
+        document.addEventListener('keydown', arrowKeyPressHandler);
+    }
+
+    
+
+    $('.searcher').on('click', function(e) {
+        if ($(e.target).closest('a').length == 0) {
+            
             e.preventDefault();
             e.stopPropagation();
         }
-        
     })
 
     $('.btn-show-results').on('click', function(e) {
@@ -29,9 +86,12 @@
     });
 
     $('.ac-search').on('focus', function() {
-        $('.ac-search-results-panel').show();
-        addEventClickOutsideSearch();
-    })
+        showSearchresults();
+    });
+
+    $('.searcher').on('mouseover', '.search-results-item', function(e) {
+        focusSearchResult($(this));
+    });
 
     $('.ac-search').on('input', function() {
         var operationsApiUrl = 'https://www3.gobiernodecanarias.org/istac/api/operations/v1.0/operations.json';
@@ -40,13 +100,14 @@
             method: 'GET',
             url: operationsApiUrl + '?query=' + (this.value? 'TITLE ILIKE "' + this.value + '"': '') ,
             success: function(response) {
-                var total = response.total;
+                $searchResultFocused = null;
+                totalSearchResult = response.total;
                 var htmlContent = '<ul class="ac-search-results">';
-                if (total > 0) {
-                    for (var i=0; i < total; i++) {
+                if (totalSearchResult > 0) {
+                    for (var i=0; i < totalSearchResult; i++) {
                         var operation = response.operation[i];
                         htmlContent += '<li class="search-results-item" data-iresult="' + i + '">';
-                            htmlContent += '<a href="./operations/'+ operation.id + '">' + getTranslatedText(operation.name) + '</a>';
+                            htmlContent += '<a class="link" href="./operations/'+ operation.id + '">' + getTranslatedText(operation.name) + '</a>';
                         htmlContent += '</li>'
                     }
                     
@@ -57,9 +118,10 @@
 
                 htmlContent += '</ul>';
 
-                $('.ac-search-results-panel').show().html(htmlContent);
+                $('.ac-search-results-panel').html(htmlContent);
             },
             error: function(e) {
+                totalSearchResult = 0;
                 console.log('Error al consultar las operaciones');
                 console.error(e);
             }
