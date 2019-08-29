1cd8f5d3fb5d21ce39fcfb603d982faaf76301df
amonfue <amonfue@arte-consultores.com>
2019-08-29 08:40:50 +0100

    METAMAC-2968 - Esqueleto inicial de página de detalle de una... ...operación estadística
    > Se añaden los meta tags de descripción y palabras clave a las páginas
    de operación estadística e instancia.
    
    > Se mejoran los titulos de las páginas comentadas y de la área
    temática.
    
    > Se actualiza la descripción de los ficheros de configuración.
    
    > Se añade la configuración del ambiente de desarrollo.

--- src/main/java/es/gobcan/istac/statistical/operations/external/service/HtmlService.java
+++ src/main/java/es/gobcan/istac/statistical/operations/external/service/HtmlService.java
@@ -1,8 +1,15 @@
 package es.gobcan.istac.statistical.operations.external.service;
 
+import org.siemac.metamac.rest.statistical_operations.v1_0.domain.Instance;
+import org.siemac.metamac.rest.statistical_operations.v1_0.domain.Operation;
+
 public interface HtmlService {
 
     public String getHeaderHtml();
 
     public String getFooterHtml();
+
+    public String getMetaKeywords(Operation operation);
+
+    public String getMetaKeywords(Instance instance);
 }
--- src/main/java/es/gobcan/istac/statistical/operations/external/service/impl/HtmlServiceImpl.java
+++ src/main/java/es/gobcan/istac/statistical/operations/external/service/impl/HtmlServiceImpl.java
@@ -1,5 +1,12 @@
 package es.gobcan.istac.statistical.operations.external.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.siemac.metamac.rest.common.v1_0.domain.Resource;
+import org.siemac.metamac.rest.statistical_operations.v1_0.domain.Instance;
+import org.siemac.metamac.rest.statistical_operations.v1_0.domain.Operation;
+import org.siemac.metamac.rest.statistical_operations.v1_0.domain.SecondarySubjectAreas;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import es.gobcan.istac.statistical.operations.external.annotation.NotLogging;
 import es.gobcan.istac.statistical.operations.external.service.HtmlService;
 import es.gobcan.istac.statistical.operations.external.service.MetadataService;
+import es.gobcan.istac.statistical.operations.external.web.util.LanguageUtil;
 
 @Service("htmlService")
 @NotLogging
@@ -19,6 +27,9 @@ public class HtmlServiceImpl implements HtmlService {
     @Autowired
     private MetadataService metadataService;
 
+    @Autowired
+    private LanguageUtil languageUtil;
+
     @Override
     public String getHeaderHtml() {
         return getHtml(metadataService.getNavbarUrl());
@@ -27,6 +38,33 @@ public class HtmlServiceImpl implements HtmlService {
     @Override
     public String getFooterHtml() {
         return getHtml(metadataService.getFooterUrl());
+    }
+
+    @Override
+    public String getMetaKeywords(Operation operation) {
+        List<String> keywords = new ArrayList<>();
+        keywords.add(languageUtil.getTraduction(operation.getSubjectArea().getName()));
+
+        if (operation.getAcronym() != null) {
+            keywords.add(languageUtil.getTraduction(operation.getAcronym()));
+        }
+
+        SecondarySubjectAreas secondarySubjectAreas = operation.getSecondarySubjectAreas();
+        if (secondarySubjectAreas != null) {
+            for (Resource resource : secondarySubjectAreas.getSecondarySubjectAreas()) {
+                keywords.add(languageUtil.getTraduction(resource.getName()));
+            }
+        }
+        return String.join(",", keywords);
+    }
+
+    @Override
+    public String getMetaKeywords(Instance instance) {
+        List<String> keywords = new ArrayList<>();
+
+        keywords.add(languageUtil.getTraduction(instance.getStatisticalOperation().getName()));
+
+        return String.join(",", keywords);
     }
 
     private String getHtml(String urlToRead) {
